65af672a7dbaf79ec1aaebeb7a334f7b97ee3879
clementgutu <100708255+clementgutu@users.noreply.github.com>
2022-10-21 12:07:02 +0200

    Add files via upload

---
+++ ProtocoleChiffrementACle.java
@@ -0,0 +1,6 @@
+
+public interface ProtocoleChiffrementACle extends ProtocoleChiffremment
+{
+	public abstract void setCle(String cle);
+	public abstract String getCle();
+}
---
+++ TranspositionInverse.java
@@ -0,0 +1,19 @@
+
+/**
+ * Chiffre les messages en inversant l'ordre des lettres.
+ */
+
+public class TranspositionInverse implements ProtocoleChiffremment
+{
+
+	@Override
+	public String chiffre(String message) 
+	{
+	}
+
+	@Override
+	public String dechiffre(String message) 
+	{
+	}
+	
+}
